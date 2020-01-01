1e06599f89564905bf7eea39552a3dfa904d6bd3
itsezc <33750251+itsezc@users.noreply.github.com>
2020-01-01 02:31:26 +0000

    [server] init and apollo server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import { ApolloServer } from 'apollo-server'
+
+import typeDefs from 'schema.graphql'
+import { resolvers } from './resolvers'
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	dataSources: () => {
+		return {
+			NLU: new NLU()
+		}
+	}
+})
+
+server
+	.listen({ port: 8086 })
+	.then((info) => console.log(`Server started on https://localhost:${info.port}`))
