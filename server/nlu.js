d8b9300e35a0f324b698b6bd3608d4613206d8bc
itzezc <chirub@foretag.co>
2020-01-04 22:30:27 +0000

    [server] NLU rest header token

--- server/nlu.js
+++ server/nlu.js
@@ -7,6 +7,10 @@ export default class NLU extends RESTDataSource {
 		this.baseURL = 'https://api.wit.ai/'
 	}
 
+	willSendRequest(request) {
+		request.headers.set('Authorization', `Bearer XU5FSZX6L2LQSUTNTD3VE4EE3IQ26WKJ`)
+	}
+
 	async message(query) {
 
 		return this.get(
