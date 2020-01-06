99ba65566d7017c0c52bbe6433d6f6546e9fb73a
itzezc <chirub@foretag.co>
2020-01-06 04:21:29 +0000

    [schema] resolver for NLU message

--- server/resolvers.js
+++ server/resolvers.js
@@ -2,9 +2,25 @@ export const resolvers = {
 	Query: {
 		message: async (parent, { query }, { dataSources }, info) => {
 
-			console.log(await dataSources.NLU.message(query))
+			const data = await dataSources.NLU.message(query)
 
-			return dataSources.NLU.message(query)
+			const { _text, msg_id, entities } = data 
+
+			const newEntities = []
+
+			for (const entity in entities) {
+				newEntities.push({
+					type: entity,
+					confidence: entities[entity][0].confidence,
+					value: entities[entity][0].value
+				})
+			}
+
+			return {
+				id: msg_id,
+				text: _text,
+				entities: newEntities
+			}
 
 		}
 	}
