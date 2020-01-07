90587a6637f8e3a4ebc152ca769a2688eb14008e
itzezc <chirub@foretag.co>
2020-01-07 12:13:46 +0000

    [server] Query resolver

---
+++ server/resolvers/query.js
@@ -0,0 +1,24 @@
+export const queryResolver = {
+
+	message: async (parent, { query }, { dataSources }, info) => {
+
+		const { _text, msg_id, entities } = await dataSources.NLU.message(query)
+
+		const newEntities = []
+
+		for (const entity in entities) {
+			newEntities.push({
+				type: entity,
+				confidence: entities[entity][0].confidence,
+				value: entities[entity][0].value
+			})
+		}
+
+		return {
+			id: msg_id,
+			text: _text,
+			entities: newEntities
+		}
+	}
+
+}
