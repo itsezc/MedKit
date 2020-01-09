3474eda85f52a54f5d456960afbbe2c443b8fc6a
itzezc <chirub@foretag.co>
2020-01-09 01:05:14 +0000

    [app] remove RN default styling

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -24,16 +24,6 @@ const GET_CHAT_RESPONSE = gql`
 	}
 `
 
-import {
-	Image,
-	Platform,
-	ScrollView,
-	StyleSheet,
-	Text,
-	TouchableOpacity,
-	View,
-} from 'react-native'
-
 import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
@@ -44,7 +34,7 @@ export default function HomeScreen() {
 		onCompleted: (data) => data.handleMessage ? addToMessageBoard([ data.handleMessage ]) : null	
 	})
 
-	const addToMessageBoard = (newMessages = []) => setMessages(prevMessages => GiftedChat.append(prevMessages, newMessages))
+	const addToMessageBoard = newMessages => setMessages(prevMessages => GiftedChat.append(prevMessages, newMessages))
 
 	const sendMessage = async (newMessages = []) => {
 
@@ -70,98 +60,9 @@ export default function HomeScreen() {
 				_id: 1
 			}}
 		/>
-	);
+	)
 }
 
 HomeScreen.navigationOptions = {
 	header: null,
 };
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-	},
-	developmentModeText: {
-		marginBottom: 20,
-		color: 'rgba(0,0,0,0.4)',
-		fontSize: 14,
-		lineHeight: 19,
-		textAlign: 'center',
-	},
-	contentContainer: {
-		paddingTop: 30,
-	},
-	welcomeContainer: {
-		alignItems: 'center',
-		marginTop: 10,
-		marginBottom: 20,
-	},
-	welcomeImage: {
-		width: 100,
-		height: 80,
-		resizeMode: 'contain',
-		marginTop: 3,
-		marginLeft: -10,
-	},
-	getStartedContainer: {
-		alignItems: 'center',
-		marginHorizontal: 50,
-	},
-	homeScreenFilename: {
-		marginVertical: 7,
-	},
-	codeHighlightText: {
-		color: 'rgba(96,100,109, 0.8)',
-	},
-	codeHighlightContainer: {
-		backgroundColor: 'rgba(0,0,0,0.05)',
-		borderRadius: 3,
-		paddingHorizontal: 4,
-	},
-	getStartedText: {
-		fontSize: 17,
-		color: 'rgba(96,100,109, 1)',
-		lineHeight: 24,
-		textAlign: 'center',
-	},
-	tabBarInfoContainer: {
-		position: 'absolute',
-		bottom: 0,
-		left: 0,
-		right: 0,
-		...Platform.select({
-			ios: {
-				shadowColor: 'black',
-				shadowOffset: { width: 0, height: -3 },
-				shadowOpacity: 0.1,
-				shadowRadius: 3,
-			},
-			android: {
-				elevation: 20,
-			},
-		}),
-		alignItems: 'center',
-		backgroundColor: '#fbfbfb',
-		paddingVertical: 20,
-	},
-	tabBarInfoText: {
-		fontSize: 17,
-		color: 'rgba(96,100,109, 1)',
-		textAlign: 'center',
-	},
-	navigationFilename: {
-		marginTop: 5,
-	},
-	helpContainer: {
-		marginTop: 15,
-		alignItems: 'center',
-	},
-	helpLink: {
-		paddingVertical: 15,
-	},
-	helpLinkText: {
-		fontSize: 14,
-		color: '#2e78b7',
-	},
-});
