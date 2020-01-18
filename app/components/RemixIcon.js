9601ce5c188c9302df2778ca189221e88ae5f709
itzezc <chirub@foretag.co>
2020-01-18 01:16:39 +0000

    [app] Cached version of RemixIcon font component

---
+++ app/components/RemixIcon.js
@@ -0,0 +1,18 @@
+/**
+ * RemixIcon icon set component.
+ * Usage: <RemixIcon name="icon-name" size={20} color="#4F8EF7" />
+ */
+
+import createIconSet from 'react-native-vector-icons/lib/create-icon-set';
+const glyphMap = {};
+
+const iconSet = createIconSet(glyphMap, 'remixicon', 'RemixIcon.ttf');
+
+export default iconSet;
+
+export const Button = iconSet.Button;
+export const TabBarItem = iconSet.TabBarItem;
+export const TabBarItemIOS = iconSet.TabBarItemIOS;
+export const ToolbarAndroid = iconSet.ToolbarAndroid;
+export const getImageSource = iconSet.getImageSource;
+
