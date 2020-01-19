c4d7bbd66678bbb2ef68b85b1242cf14331824ab
itzezc <chirub@foretag.co>
2020-01-19 07:03:13 +0000

    [app] Profile #2

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,23 +1,24 @@
 import React from 'react'
+
 import { Container } from '../components/styles/Container'
 import { Card } from '../components/styles/Card'
 import { CardTitle } from '../components/styles/CardTitle'
+
+import { Switch } from 'react-native'
 
 export default Profile = () => {
 	return(
 		<Container>
 			<Card >
-			<CardTitle >Card number 1</CardTitle>
+				<CardTitle >Card number 1</CardTitle>
 			</Card>
 			<Card >
-			<CardTitle >Card number 2</CardTitle>
+				<CardTitle >Card number 2</CardTitle>
 			</Card>
 			<Card >
-			<CardTitle >Card number 3</CardTitle>
-			</Card>
-			<Card >
-			<CardTitle >Card number  4</CardTitle>
+				<CardTitle >Card number 3</CardTitle>
 			</Card>
 		</Container>
 	)
 }
+
