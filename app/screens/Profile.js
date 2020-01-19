f3c5040b4aa4a90f59233905d4b5443cf6a05167
itzezc <chirub@foretag.co>
2020-01-19 23:55:36 +0000

    [app] Profile settings widget switch demo

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -6,6 +6,7 @@ import { Card } from '../components/styles/Card'
 import { CardTitle } from '../components/styles/CardTitle'
 
 import { View, Text, Image } from 'react-native'
+import Switch from '../components/Switch'
 import Styled from 'styled-components/native'
 
 const Back = Styled.View`
@@ -40,6 +41,33 @@ const List = (props) => (
 		>
 			{props.text}
 		</Text>
+		<Switch
+			defaultValue={true}
+			activeText={''}
+			inactiveText={''}
+			fontSize={16}
+			activeTextColor={'rgba(255, 255, 255, 1)'}
+			inactiveTextColor={'rgba(255, 255, 255, 1)'}
+			activeBackgroundColor={'rgba(50, 163, 50, 1)'}
+			inactiveBackgroundColor={'rgba(137, 137, 137, 1)'}
+			activeButtonBackgroundColor={'rgba(255, 255, 255, 1)'}
+			inactiveButtonBackgroundColor={'rgba(255, 255, 255, 1)'}
+			switchWidth={70}
+			switchHeight={30}
+			switchBorderRadius={0}
+			switchBorderColor={'rgba(0, 0, 0, 1)'}
+			switchBorderWidth={0}
+			buttonWidth={25}
+			buttonHeight={25}
+			buttonBorderRadius={0}
+			buttonBorderColor={'rgba(0, 0, 0, 1)'}
+			buttonBorderWidth={0}
+			animationTime={150}
+			padding={true}
+			onChangeValue={(value) => {
+				console.log(value);
+			}}
+          />
 	</View>
 )
 
