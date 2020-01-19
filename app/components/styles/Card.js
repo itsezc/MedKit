046945b084debf88440c9d7020cd45346b05e6bb
itzezc <chirub@foretag.co>
2020-01-19 07:03:20 +0000

    [app] Card #2

--- app/components/styles/Card.js
+++ app/components/styles/Card.js
@@ -3,12 +3,11 @@ import { StyleSheet, View } from 'react-native'
 import Style from 'styled-components/native'
 
 export const Card = Style.View`
-	flex: 1;
 	border: 1px solid #ccc;
 	margin: 2px 0;
 	border-radius: 10px;
-	box-shadow: 0 0 10px #ccc;
-	background-color: #fff;
+	background-color: #FFFFFF;
 	width: 80%;
 	padding: 10px;
 `
+
