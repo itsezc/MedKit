92ae2adf84fa650b1c3aa71426f299cecbb42d34
itzezc <chirub@foretag.co>
2020-01-19 05:12:19 +0000

    [app] Card Title component update fix export

--- app/components/styles/CardTitle.js
+++ app/components/styles/CardTitle.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StyleSheet, Text } from 'react-native'
 import Style from 'styled-components'
 
-export const Title = Style.Text`
+export const CardTitle = Style.Text`
 	font-size: 20px;
 	text-align: center;
 	color: red;
