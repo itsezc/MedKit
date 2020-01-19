a70de2296120db0991156419b9961b36dd4eaa15
itzezc <chirub@foretag.co>
2020-01-19 07:03:27 +0000

    [app] Container #3

--- app/components/styles/Container.js
+++ app/components/styles/Container.js
@@ -1,11 +1,17 @@
 import React from 'react'
+
 import { StyleSheet, View } from 'react-native'
 import Style from 'styled-components/native'
+
+import RainBG from '../../assets/images/RainBG.svg'
+// https://loading.io/pattern/m-rain
 
 export const Container = Style.View`
 	flex: 1;
 	padding: 50px 0;
 	justify-content: center;
-	background-color: #f4f4f4;
+	background-color: #2276df;
+	background-image: url(${RainBG});
 	align-items: center;
 `
+
