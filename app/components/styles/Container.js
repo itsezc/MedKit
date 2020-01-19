ef38bbcc3608d689427479a9c9e48d7b3de6dd8d
itzezc <chirub@foretag.co>
2020-01-19 05:07:06 +0000

    [app] Container styled component (base)

---
+++ app/components/styles/Container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Style from 'styled-components'
+
+export const Container = Style.View`
+	flex: 1;
+	padding: 50px 0;
+	justify-content: center;
+	background-color: #f4f4f4;
+	align-items: center;
+`
