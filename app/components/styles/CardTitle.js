2691901857793650f9770c658db5421a403360cb
itzezc <chirub@foretag.co>
2020-01-19 05:08:51 +0000

    [app] Title styled component (base)

---
+++ app/components/styles/CardTitle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { StyleSheet, Text } from 'react-native'
+import Style from 'styled-components'
+
+export const Title = Style.Text`
+	font-size: 20px;
+	text-align: center;
+	color: red;
+`
