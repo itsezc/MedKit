65c008e01b78a958e8c8a83ccbc13e0391866c7d
itzezc <chirub@foretag.co>
2020-01-19 05:07:32 +0000

    [app] Card styled component (base)

---
+++ app/components/styles/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import Style from 'styled-components'
+
+export const Card = Style.View`
+	flex: 1;
+	border: 1px solid #ccc;
+	margin: 2px 0;
+	border-radius: 10px;
+	box-shadow: 0 0 10px #ccc;
+	background-color: #fff;
+	width: 80%;
+	padding: 10px;
+`
--- app/components/styles/Container.js
+++ app/components/styles/Container.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import Style from 'styled-components'
 
 export const Container = Style.View`
