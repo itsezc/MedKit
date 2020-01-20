7bdd3c9a24eae7e1268e02acf3b8dce4f97247a3
itzezc <chirub@foretag.co>
2020-01-20 20:05:20 +0000

    [server] Authorization context w/ JWT token

--- server/index.js
+++ server/index.js
@@ -3,10 +3,21 @@ import { ApolloServer } from 'apollo-server'
 import NLU from './nlu'
 
 import { schema } from './database/generateSchema'
+import { getUser } from './core/getUser'
 
 const server = new ApolloServer({
 	schema,
-	context: ({ req }) => req,
+	context: ({ req }) => {
+		
+		const authorization = req.headers.authorization
+		
+		const user = getUser(authorization)
+
+		return {
+			user
+		}
+
+	},
 	dataSources: () => {
 		return {
 			NLU: new NLU()
