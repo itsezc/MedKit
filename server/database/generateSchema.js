fef38c010ef9807cc2e30507b21a7bee392690ba
itzezc <chirub@foretag.co>
2020-01-20 22:55:04 +0000

    [server] Decouple merge schemas

--- server/database/generateSchema.js
+++ server/database/generateSchema.js
@@ -1,27 +1,20 @@
 // @flow
 
 import appTypeDefs from './schemas/app.graphql'
-import { schema as dbSchema } from './database'
-import { mergeSchemas, makeExecutableSchema } from 'apollo-server'
-
 import { Date } from '../resolvers/scalars'
 import { queryResolver as Query } from '../resolvers/query'
 import { mutationResolver as Mutation } from '../resolvers/mutation'
 
-const appSchema = makeExecutableSchema({ typeDefs: appTypeDefs, resolvers: {} })
+import { schema as cruddlSchema } from './database'
 
-const schema = mergeSchemas({
-	schemas: [
-		dbSchema,
-		appSchema
-	], 
-	resolvers: {
-		Query,
-		Mutation,
-		Date
-	}
-})
+const appResolvers = {
+	Query,
+	Mutation,
+	Date
+}
 
 export {
-	schema
+	appTypeDefs,
+	appResolvers,
+	cruddlSchema
 }
