f28fbb3c4d1d0fcb52489caf4d16ff77c9b896be
itzezc <chirub@foretag.co>
2020-01-22 07:30:30 +0000

    [app] Home screen diagonal style demo

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,15 +1,71 @@
 import React from 'react'
 
 import { Screen } from '../components/styles/Screen'
-import { View, Text, Button } from 'react-native'
+import { TouchableWithoutFeedback, Dimensions, View, Text, Button, Image } from 'react-native'
+
+import Styled from 'styled-components/native'
 
 export default (props) => {
 	return(
-		<Screen>
-			<Button
-				title='Profile'
-				onPress={() => props.navigation.push('Profile')}
+		<Screen
+			style={{
+				padding: 0,
+				maxHeight: 180
+			}}
+		>
+
+			<View
+				style={{
+					flex: 1,
+					marginLeft: 30,
+					marginTop: 50,
+				}}
+			>
+				<Text style={{ color: '#F5F7FD', fontSize: 24 }}>Hello,</Text>
+				<Text style={{ color: '#FFFFFF', fontSize: 34, fontWeight: 'bold' }}>Chiru</Text>
+				<TouchableWithoutFeedback
+					onPress={() => props.navigation.push('Profile')}
+				>
+					<Image
+						style={{ 
+							width: 55, 
+							height: 55, 
+							borderRadius: 30,
+							position: 'absolute',
+							right: 30,
+						}}
+						source='https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg'
+					/>
+				</TouchableWithoutFeedback>
+			</View>
+
+			<View
+				style={{
+					position: 'absolute',
+					backgroundColor: 'transparent',
+					borderStyle: 'solid',
+					borderLeftWidth: Dimensions.get('window').width,
+					borderRightWidth: 0,
+					borderBottomWidth: 50,
+					borderLeftColor: 'transparent',
+					borderRightColor: 'transparent',
+					borderBottomColor: '#fff',
+					marginTop: 130,
+				}}
 			/>
+
+			<View
+				style={{
+					width: '100%',
+					height: '100%',
+					backgroundColor: '#fff',
+					marginTop: 150,
+				}}	
+			>
+				<Text>
+					Content
+				</Text>
+			</View>
 		</Screen>
 	)
 }
