15b24736c49821a0631b9aec8ed844f934431518
itzezc <chirub@foretag.co>
2020-01-22 06:26:16 +0000

    [app] Home screen with BG

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,11 +5,11 @@ import { View, Text, Button } from 'react-native'
 
 export default (props) => {
 	return(
-		<View>
+		<Screen>
 			<Button
 				title='Profile'
 				onPress={() => props.navigation.push('Profile')}
 			/>
-		</View>
+		</Screen>
 	)
 }
