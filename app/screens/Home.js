3691b7212b1f9435955d749d9c42d70d3aa1078f
itzezc <chirub@foretag.co>
2020-01-22 22:24:58 +0000

    [app] Home Screen filter list base component

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -21,6 +21,29 @@ export const MiniCard = Styled.View`
 	shadow-color: #000;
 `
 
+export const FilterContainer = Styled.View`
+	border-radius: 5px;
+	margin: 0 10px 0 0px;
+	padding: 10px 20px;
+`
+
+export const FilterItem = (props) => (
+	<FilterContainer
+		style={{
+			backgroundColor: props.selected ? '#2276DF' : '#FFFFFF'
+		}}	
+	>
+		<Text
+			style={{
+				color: props.selected ? '#FFFFFF' : '#000000',
+				fontWeight: props.selected ? 'bold' : 'normal'
+			}}
+		>
+			{props.item}
+		</Text>
+	</FilterContainer>
+)
+
 
 export const MinCardContainer = (props) => (
 	<View
@@ -137,9 +160,44 @@ export default (props) => {
 					paddingTop: 50,
 					top: -50,
 					minHeight: '100%'
-
 				}}
+
+				wrapper
 			>
+				<View
+					style={{
+						marginTop: 35
+					}}
+				>
+					<ScrollView
+						style={{
+							minWidth: '100%',
+							minHeight: '110%',
+							paddingLeft: 20,
+							paddingRight: 30
+						}}
+						horizontal
+					>
+						<FilterItem
+							item='All'
+							selected
+						/>
+
+						<FilterItem
+							item='Appointments'
+						/>
+
+						<FilterItem
+							item='Medication'
+						/>
+
+						<FilterItem
+							item='Exercises'
+						/>
+
+					</ScrollView>
+
+				</View>
 			</View>
 
 			<View
