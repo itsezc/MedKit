c463f161d4293fdce74ba5acf31208ce4bedb831
itzezc <chirub@foretag.co>
2020-01-22 06:00:36 +0000

    [app] Base screen for home component

---
+++ app/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Screen } from '../components/styles/Screen'
+import { View, Text } from 'react-native'
+
+export default () => {
+	return(
+		<View>
+			<Text>
+				LOL
+			</Text>
+		</View>
+	)
+}
