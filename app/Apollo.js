b0e3cbd3e781c2591fca4a6b6d104ca79864d5a9
itzezc <chirub@foretag.co>
2020-01-26 07:00:21 +0000

    [app] Apollo Link context with Auth

--- app/Apollo.js
+++ app/Apollo.js
@@ -1,11 +1,28 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
+
+import { AsyncStorage as Storage } from 'react-native'
 
 const cache = new InMemoryCache()
-const link = new HttpLink({
+
+const httpLink = new HttpLink({
 	uri: 'http://localhost:8086/graphql'
 })
+
+const middlewareLink = new ApolloLink(async(operation, forward) => {
+
+	operation.setContext({
+		headers: {
+			authorization: await Storage.getItem('token') || null
+		}
+	})
+
+	return forward(operation)
+})
+
+const link = middlewareLink.concat(httpLink)
 
 export const client = new ApolloClient({
 	cache,
