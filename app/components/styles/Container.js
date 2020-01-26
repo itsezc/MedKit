56b1b6fbe807cedc1d7f0e81abcfc22f2add6a1d
itzezc <chirub@foretag.co>
2020-01-26 05:34:02 +0000

    [app] Container styling padding from Bottom

--- app/components/styles/Container.js
+++ app/components/styles/Container.js
@@ -6,8 +6,9 @@ import Style from 'styled-components/native'
 
 export const Container = Style.View`
 	display: flex;
-	padding: 100px 30px;
+	padding: 100px 30px 50px 30px;
 	justify-content: center;
 	align-items: stretch;
+	height: 100vh;
 `
 
