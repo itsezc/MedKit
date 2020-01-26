f883646ec809e5b3de77684835e36b77e4831fef
itzezc <chirub@foretag.co>
2020-01-26 06:12:03 +0000

    [app] BUG fix export AsyncFactory

--- app/storage.js
+++ app/storage.js
@@ -1,6 +1,6 @@
-import AsyncStorageFactor from '@react-native-community/async-storage'
+import AsyncStorageFactory from '@react-native-community/async-storage'
 import LegacyStorage from '@react-native-community/async-storage-backend-legacy'
 
 const legacyStorage = new LegacyStorage()
 
-const const Storage = AsyncStorageFactor.create(legacyStorage)
+export const Storage = AsyncStorageFactory.create(legacyStorage)
