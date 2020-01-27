afc77b0641cd8be7b0c9aedfb34ec1a9eb3980a4
itzezc <chirub@foretag.co>
2020-01-27 13:26:33 +0000

    [app] Chatbox bubble component styling

--- app/screens/Chat.js
+++ app/screens/Chat.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-import { GiftedChat as Chat } from 'react-native-gifted-chat'
+import { GiftedChat as Chat, Bubble } from 'react-native-gifted-chat'
 import { View, Text } from 'react-native'
 
 import { useLazyQuery, gql } from '@apollo/client'
@@ -27,41 +27,29 @@ export default function HomeScreen(props) {
 
 	const [messages, setMessages] = useState([
 		{
-			_id: 2,
-			text: 'This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT',
+			_id: 3,
+			text: 'User - This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT',
 			createdAt: new Date(),
-			quickReplies: {
-				type: 'radio', // or 'checkbox',
-				keepIt: true,
-				values: [
-				{
-				title: '😋 Yes',
-				value: 'yes',
-				},
-				{
-				title: '📷 Yes, let me show you with a picture!',
-				value: 'yes_picture',
-				},
-				{
-				title: '😞 Nope. What?',
-				value: 'no',
-				},
-				],
-			},
 			user: {
-				_id: 1,
-				name: 'React Native',
+				_id: 1
 			},
 		},
 		{
-			_id: 1,
-			text: 'This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT',
+			_id: 2,
+			text: 'Server - This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT',
 			createdAt: new Date(),
 			user: {
-				_id: 2,
-				name: 'React Native',
+				_id: 2
 			},
-		}		
+		}	,
+		{
+			_id: 1,
+			text: 'Server - This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT',
+			createdAt: new Date(),
+			user: {
+				_id: 2
+			},
+		}	
 	])
 
 	const [getChatResponse, { loading, data }] = useLazyQuery(GET_CHAT_RESPONSE, {
@@ -89,16 +77,15 @@ export default function HomeScreen(props) {
 		<View
 			style={{
 				flex: 1,
-				backgroundColor: '#2276DF'
+				backgroundColor: '#2B8FFF'
 			}}
 		>
 			<View 
 				style={{
-					backgroundColor: '#2276DF',
 					height: '80px',
 					justifyContent: 'center',
 					alignItems: 'center',
-					shadowOpacity: 0.35,
+					shadowOpacity: 0.25,
 					shadowOffset: {
 						width: 0,
 						height: 0
@@ -126,6 +113,42 @@ export default function HomeScreen(props) {
 				renderAvatar={null}
 				renderTime={() => false}
 				renderDay={() => false}
+				renderBubble ={(props) => {
+
+					// const user = props.messages.map(message => message.user._id)
+
+					return (
+						<Bubble
+							{...props}
+							textStyle={{
+								right: {
+									color: '#00406B',
+									margin: 0
+								},
+								left: {
+									color: '#FFFFFF',
+									margin: 0
+								}
+							}}
+							wrapperStyle={{
+								left: {
+									backgroundColor: '#2380E9',
+									borderTopLeftRadius: 0,
+									paddingVertical: 20,
+									paddingHorizontal: 25,
+									marginBottom: 5
+								},
+								right: {
+									backgroundColor: '#FFFFFF',
+									borderTopRightRadius: 0,
+									paddingVertical: 20,
+									paddingHorizontal: 25,
+									marginBottom: 5
+								}
+							}}
+						/>
+					)
+				}}
 			/>
 		</View>
 	)
