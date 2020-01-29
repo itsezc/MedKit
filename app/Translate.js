a45e1828e5894309833863e476389941e74ff005
itzezc <chirub@foretag.co>
2020-01-29 11:41:07 +0000

    [app] Translate - findBestAvaliableLanguage

--- app/Translate.js
+++ app/Translate.js
@@ -3,19 +3,26 @@ import * as Localize from 'react-native-localize'
 import i18N from 'i18next'
 import { initReactI18next } from 'react-i18next'
 
+import LANG_EN from './assets/languages/en.json'
+import LANG_ES from './assets/languages/es.json'
+import LANG_HI from './assets/languages/hi.json'
+import LANG_TA from './assets/languages/ta.json'
+
 const resources = {
-	en: {
-		translation: {
-			"Hello": "hello",
-		}
-	}
+	en: LANG_EN,
+	es: LANG_ES,
+	hi: LANG_HI,
+	ta: LANG_TA
 }
+
+const fallback = { languageCode: 'en', isRTL: false }
+const { languageTag, isRTL } = Localize.findBestAvailableLanguage(Object.keys(resources))
 
 i18N
 	.use(initReactI18next)
 	.init({
 		resources,
-		lng: 'en',
+		lng: languageTag,
 		keySeparator: false,
 		interpolation: {
 			escapeValue: false
