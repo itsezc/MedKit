7f4ae9f2f5d140e7702f50e28c48241cf4350bec
itzezc <chirub@foretag.co>
2020-02-08 10:37:45 +0000

    [app] Apollo Client new NPM org

--- app/Apollo.js
+++ app/Apollo.js
@@ -1,6 +1,4 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 import { setContext } from 'apollo-link-context'
 
 import { AsyncStorage as Storage } from 'react-native'
