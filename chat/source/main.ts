0913b8e9ac743d53ed028941a50d87747fe46fc3
itzezc <chirub@foretag.co>
2020-02-15 11:35:25 +0000

    [chat] Server initialize

--- chat/source/main.ts
+++ chat/source/main.ts
@@ -9,3 +9,7 @@ import typeDefs from '../schema.graphql'
 const Server: ApolloServer = new ApolloServer({
 	schema: buildFederatedSchema([ { typeDefs: gql`${typeDefs}`, resolvers } ])
 })
+
+Server
+	.listen({ port: 8084 })
+	.then(( info ) => console.log(`[FEDERATION] Chat server started on port - ${info.port}`))
