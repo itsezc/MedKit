eb3aaf76630b0c0b9cdda0099f054b62b8a4b186
itzezc <chirub@foretag.co>
2020-02-15 18:40:57 +0000

    [chat] -OLD- w/ Queries for Symptom identification

--- chat/source/library/identify/index.ts
+++ chat/source/library/identify/index.ts
@@ -1,57 +1,58 @@
 import { query } from '../GClient'
 
-// Example: cold
+// Example: cold (can be both a symptom and disease - i.e. Common Cold)
+
 export default async function(parent, { term }, context, info) {
 
-	// const QUERY_DISEASES = gql`
-	// 	query findDisease($term: String!) {
-	// 		allDiseases(
-	// 			filter: {
-	// 				name_like: $term
-	// 			}
-	// 		) {
-	// 			name
-	// 		}
-	// 	}
-	// `
+	const QUERY_DISEASES = gql`
+		query findDisease($term: String!) {
+			allDiseases(
+				filter: {
+					name_like: $term
+				}
+			) {
+				name
+			}
+		}
+	`
 	
-	// const { allDiseases } = await query(QUERY_DISEASES, { term })
+	const { allDiseases } = await query(QUERY_DISEASES, { term })
 
-	// if (allDiseases.name) {
-	// 	return {
-	// 		diseases: allDiseases.name // An array of names whether its 0, 1, 100
-	// 	}
-	// }
+	if (allDiseases.name) {
+		return {
+			diseases: allDiseases.name // An array of names whether its 0, 1, 100
+		}
+	}
 
 	/** 
 		* Find any symptoms with the search term in the database
 	*/
 
-	// const QUERY_SYMPTOMS = gql`
-	// 	query findSymptom($term: String) {
-	// 		allSymptoms(
-	// 			filter: {
-	// 				name_like: $term
-	// 			}
-	// 		) {
-	// 			name
-	// 		}
-	// 	}
-	// `	
-	// const { allSymptoms } = await query(QUERY_DISEASES, { term })
+	const QUERY_SYMPTOMS = gql`
+		query findSymptom($term: String) {
+			allSymptoms(
+				filter: {
+					name_like: $term
+				}
+			) {
+				name
+			}
+		}
+	`	
+	const { allSymptoms } = await query(QUERY_DISEASES, { term })
 
-	// if (allSymptoms.name) {
-	// 	return {
-	// 		symptoms: [allSymptoms]
-	// 	}
-	// }
-
-	return {
-		diseases: [
-			'cold',
-			'chest infection',
-			'fever',
-			'asthma'
-		]
+	if (allSymptoms.name) {
+		return {
+			symptoms: [allSymptoms]
+		}
 	}
+
+	// return {
+	// 	diseases: [
+	// 		'cold',
+	// 		'chest infection',
+	// 		'fever',
+	// 		'asthma'
+	// 	]
+	// }
 }
