a069a58ffe160719abeb42c722a4f377c7be0291
itzezc <chirub@foretag.co>
2020-03-06 17:08:46 +0000

    [chat] Frequent / Mode utility function

---
+++ chat/source/library/identify/frequent.ts
@@ -0,0 +1,8 @@
+export function Frequent(array: []) {
+	return Object.entries(array.reduce((previous: any, current: any) => {
+		if (previous[current] === undefined) previous[current] = 1
+		else previous[current]++
+		return previous
+	}, {})).reduce((previous, current) => (current[1] >= previous[1] ? current : previous))[0]
+}
+
