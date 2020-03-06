ae099375c438054f197c71028c75cbe2ef25ddb2
itzezc <chirub@foretag.co>
2020-03-06 19:50:23 +0000

    [chat] Step #2 - Remove possibiltiies from filter

--- chat/source/library/getDiseases/index.ts
+++ chat/source/library/getDiseases/index.ts
@@ -3,8 +3,9 @@ import { Frequent } from './frequent'
 export async function getDiseases(diseases: string[]) {
 	
 	const getUnique = (diseases: string[]) => diseases.filter((element) => diseases.indexOf(element) === diseases.lastIndexOf(element))
+	const removePossibilities = (diseases: string[], possibilites: string[]) => diseases.filter(disease => !possibilites.includes(disease))
 
 	const likely: string[] = []
-	const possibilites: string[] = getUnique(diseases)	
+	const possibilites: string[] = getUnique(diseases)
 
 }
