ee2ef45249f9cbb6b6f5f8bbf32d3bb63cf3c77c
itzezc <chirub@foretag.co>
2020-03-06 20:00:54 +0000

    [chat] Stage #2 - #R1 Complete

--- chat/source/library/getDiseases/index.ts
+++ chat/source/library/getDiseases/index.ts
@@ -1,11 +1,12 @@
 import { Frequent } from './frequent'
 
-export async function getDiseases(diseases: string[]) {
+export function getDiseases(diseases: string[]): string[] {
 	
 	const getUnique = (diseases: string[]) => diseases.filter((element) => diseases.indexOf(element) === diseases.lastIndexOf(element))
 	const removePossibilities = (diseases: string[], possibilites: string[]) => diseases.filter(disease => !possibilites.includes(disease))
 
-	const likely: string[] = []
 	const possibilites: string[] = getUnique(diseases)
+	const likely: string[] = Array.from(new Set(removePossibilities(diseases, possibilites)))
 
+	return likely
 }
