f71195a8d1ff886b0c3b123afd6f3407c889bd84
itzezc <chirub@foretag.co>
2020-03-07 11:37:55 +0000

    [server] Seeder delete mutations and loops

--- server/source/core/Seeder/index.js
+++ server/source/core/Seeder/index.js
@@ -2,19 +2,28 @@
 
 import { gql } from 'apollo-server'
 import { query } from '../GClient'
-import Data from '../../../seed.graphql'
+import { Data } from '../../../seed'
 
 export async function Seed(): Promise<boolean | void> {
 
-	const DELETE_ALL_DATA: GraphQL = gql``
-	
-	const seed: GraphQL = await gql(Data)
+	const DELETE_ALL_ACCOUNTS: GraphQL = `mutation { deleteAllAccounts { id } }`
+	const DELETE_ALL_DISEASES: GraphQL = `mutation { deleteAllDiseases { id } }`
+	const DELETE_ALL_SYMPTOMS: GraphQL = `mutation { deleteAllSymptoms { id } }`
 
 	try {
-		await query(seed)
+	
+		await query(DELETE_ALL_ACCOUNTS)
+		await query(DELETE_ALL_DISEASES)
+		await query(DELETE_ALL_SYMPTOMS)
+
+		Object.keys(Data).forEach(async(property) => await Data[property].forEach(async(GQuery) => console.log(await query(GQuery))))
 	} catch (e) {
 		console.error(e)
 	}
 	
+	
+
 	return true
 }
+
+Seed()
