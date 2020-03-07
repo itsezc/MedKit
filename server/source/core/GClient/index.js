b80b7a3db1aa86e22a8db084fa1566ff26bf1e94
itzezc <chirub@foretag.co>
2020-03-07 11:37:32 +0000

    [server] GClient port argument and types

--- server/source/core/GClient/index.js
+++ server/source/core/GClient/index.js
@@ -3,18 +3,23 @@
 import Request from 'axios'
 import type { DocumentNode } from 'graphql'
 
-export const fetch = Request.create({
-	method: 'post',
-	baseURL: 'http://localhost:8085',
-	timeout: 1000,
-	headers: {
-		'Accept': 'application/json',
-		'Content-Type': 'application/json'
-	},
-	responseType: 'json'
-})
+export const query = async(
+		query: string | DocumentNode, 
+		variables?: any = null, 
+		port?: number = 8085
+	) => {
 
-export const query = async(query: string | DocumentNode, variables?: any) => {
+	const fetch = Request.create({
+		method: 'post',
+		baseURL: 'http://localhost:' + port.toString(),
+		timeout: 1000,
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		responseType: 'json'
+	})
+
 	const { data } = await fetch.post('', JSON.stringify({ query, variables }))
 	return data.data
 }
