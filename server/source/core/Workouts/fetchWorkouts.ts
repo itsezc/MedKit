9aec55e1a483d41d9173ec888edde6106f8a0b68
itzezc <chirub@foretag.co>
2020-03-11 16:26:18 +0000

    [server] fetchWorkouts base #R1

---
+++ server/source/core/Workouts/fetchWorkouts.ts
@@ -0,0 +1,6 @@
+import gql from 'graphql-tag'
+import { query } from '../../util/GClient'
+
+export async function fetchWorkouts({ id }: { id: string }) {
+	
+}
---
+++ server/source/core/Workouts/index.ts
@@ -0,0 +1,1 @@
+export { fetchWorkouts } from './fetchWorkouts'
