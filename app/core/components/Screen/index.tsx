cf30ec5fc1e4e69d62bbcc30b60d776a3e5511d4
itzezc <chirub@foretag.co>
2020-03-11 15:25:10 +0000

    [chat] Basic screen component

---
+++ app/core/components/Screen/index.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import Constants from 'expo-constants'
+import Styled from 'styled-components/native'
+
+const ScreenContainer = Styled.View`
+	flex: 1;
+	background-color: #2276df;
+	paddingTop: ${Constants.statusBarHeight}px;
+`
+
+function Screen(props: { animated?: boolean, children: React.ReactElement }) {
+	return (
+		<ScreenContainer>
+			{props.children}
+		</ScreenContainer>
+	)
+}
+
+
+export { Screen }
