6a02b6a9820e9915fc1f01b2ee0d2cfc7a3a070b
itzezc <chirub@foretag.co>
2020-03-11 09:23:27 +0000

    [chat] Stage #3: Questions generation base #R1

---
+++ chat/source/library/questions/generate.ts
@@ -0,0 +1,35 @@
+import { query } from '../GClient'
+
+import gql from 'graphql-tag'
+import { DocumentNode } from 'graphql'
+
+export async function generateQuestions(diseases: string[], server: SocketIO.Server) {
+
+	const current: string = diseases[0]
+	const questions: Question[][] = []
+
+	/**
+	 * Fetch all questions based on the list of IDs provided
+	 */
+	diseases.forEach(async (disease, index) => {
+		const GET_QUESTIONS: DocumentNode = gql`
+			query {
+				allQuestions(
+					filter: {
+						diseases_some: {
+							id: $disease
+						}
+					}
+				) {
+					question
+					replies{ 
+						title
+						value
+					}
+				}
+			}
+		`
+		const { allQuestions } = await query(GET_QUESTIONS, { disease })
+		questions[index] = allQuestions
+	})
+}
