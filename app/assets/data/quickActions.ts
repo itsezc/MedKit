a9266be0213331413c7024870c6d38818a62c14d
itzezc <chirub@foretag.co>
2020-03-12 17:10:11 +0000

    [app] Assets for quickActions (TS)

---
+++ app/assets/data/quickActions.ts
@@ -0,0 +1,26 @@
+import imageCheckUp from '../images/checkUp.png'
+import imageOrderDrugs from '../images/orderDrugs.png'
+import imageExercise from '../images/exercise.png'
+import imageFood from '../images/food.png'
+
+export default {
+	"actions": [
+		{
+			"name": "CheckUp",
+			"image": imageCheckUp,
+			"link": "Chat"
+		},
+		{
+			"name": "OrderDrugs",
+			"image": imageOrderDrugs
+		},
+		{
+			"name": "Fitness",
+			"image": imageExercise
+		},
+		{
+			"name": "Food",
+			"image": imageFood
+		}
+	]
+}
