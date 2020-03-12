575052e3fc00111ee480a3457875c9dc10802b4c
itzezc <chirub@foretag.co>
2020-03-12 17:11:05 +0000

    [app] Screen component props.children types fix

--- app/core/components/Screen/index.tsx
+++ app/core/components/Screen/index.tsx
@@ -8,7 +8,8 @@ const ScreenContainer = Styled.View`
 	paddingTop: ${Constants.statusBarHeight}px;
 `
 
-function Screen(props: { animated?: boolean, children: React.ReactElement }) {
+function Screen(props: {
+	animated?: boolean, children: React.ReactElement | React.ReactElement[]}) {
 	return (
 		<ScreenContainer>
 			{props.children}
