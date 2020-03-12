fa9e46deac8b470a03931b4bdcbe825fd6bdc526
itzezc <chirub@foretag.co>
2020-03-12 17:11:27 +0000

    [app] Home screen apply components #R1.1

--- app/core/components/index.ts
+++ app/core/components/index.ts
@@ -1,3 +1,4 @@
 export { Container } from './Container'
 export { Screen } from './Screen'
 export { Calendar } from './Calendar'
+export { MiniCards } from './Home'
---
+++ app/core/screens/Home/index.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+
+import {
+	Screen,
+	Calendar,
+	MiniCards
+} from '../../components'
+import { View, Image, Text, TouchableWithoutFeedback, Dimensions } from 'react-native'
+
+import { useQuery, gql } from '@apollo/client'
+
+
+export default function ({ navigation }): JSX.Element {
+	return (
+		<Screen>
+			<View
+				style={{
+					flex: 1,
+					marginLeft: 30,
+					marginTop: 50,
+				}}
+			>
+				<Text style={{ color: '#F5F7FD', fontSize: 24 }}>Hello,</Text>
+				<Text style={{ color: '#FFFFFF', fontSize: 34, fontWeight: 'bold' }}>Chiru</Text>
+				<TouchableWithoutFeedback
+					onPress={() => navigation.navigate('Profile')}
+				>
+					<Image
+						style={{ 
+							width: 55, 
+							height: 55, 
+							borderRadius: 30,
+							position: 'absolute',
+							right: 30,
+						}}
+						source={{ uri: 'https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg'}}
+					/>
+				</TouchableWithoutFeedback>
+			</View>
+			<View
+				style={{
+					position: 'absolute',
+					backgroundColor: 'transparent',
+					borderStyle: 'solid',
+					borderLeftWidth: Dimensions.get('window').width,
+					borderRightWidth: 0,
+					borderBottomWidth: 50,
+					borderLeftColor: 'transparent',
+					borderRightColor: 'transparent',
+					borderBottomColor: '#F0F5FD',
+					marginTop: 200,
+				}}
+			/>
+			<MiniCards navigation={navigation} />
+			<Calendar />
+		</Screen>
+	)
+}
