d71ef18b161048d990c999fe1cb7fb64acd3e123
itzezc <chirub@foretag.co>
2020-03-12 14:54:44 +0000

    [app] Typings structure updates

--- app/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.svg' {
-	const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-	export default content;
-}
-  
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.gif'
-declare module '*.webp'
