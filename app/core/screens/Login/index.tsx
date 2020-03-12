986f4d9ba2481a58e5372b2f949f375011db311a
itzezc <chirub@foretag.co>
2020-03-12 10:39:49 +0000

    [app] Login screen, GQL mutation and states (Auth)

--- app/core/screens/Login/index.tsx
+++ app/core/screens/Login/index.tsx
@@ -1,8 +1,52 @@
 import * as React from 'react'
+
 import { Text, Button } from 'react-native'
+import { TextField } from 'react-native-material-textfield'
+
 import { Screen } from '../../components/Screen'
 
+import { verifyAuth, errorHandler } from '../../auth'
+import { useMutation, gql, useApolloClient } from '@apollo/client'
+
+const LOGIN_MUTATION = gql`
+	mutation Login($email: String!, $password: String!) {
+		login(email: $email, password: $password) {
+			token
+			user {
+				id
+				email
+				firstName
+				lastName
+				dateOfBirth
+				weight
+			}
+		}
+	}
+`
+
+const initialState = {
+	email = '',
+	password = '',
+	emailError = '',
+	passwordError = ''
+}
+
 export default function ({ navigation }) {
+
+	const client = useApolloClient()
+
+	const [{ email, password, emailError, passwordError }, setState] = React.useState(initialState)
+
+	const [processLogin, { loading }] = useMutation(LOGIN_MUTATION, {
+		onCompleted: async (data) => {
+			const { login: { user } } = data
+			await verifyAuth(data)
+			setState({ ...initialState })
+			navigation.navigate('Home')
+		},
+		onError: async(error) => errorHandler(error, setState)
+	})
+
 	return (
 		<Screen>
 			<Button
@@ -11,4 +55,4 @@ export default function ({ navigation }) {
 			/>
 		</Screen>
 	)
-}
+
