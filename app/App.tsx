5e63b2a472ef4fbf72424eb78afeea37e188592f
itzezc <chirub@foretag.co>
2020-03-12 14:33:00 +0000

    [app] Async resource loading (App.tsx)

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler'
 
-import React from 'react'
+import * as React from 'react'
 import { Platform, StatusBar } from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native'
@@ -9,7 +9,23 @@ import { Navigation } from './core/navigation'
 import { ApolloProvider } from '@apollo/client'
 import { client } from './core/apollo'
 
-export default function App() {
+import { AppLoading } from 'expo'
+import { Asset } from 'expo-asset'
+import * as Font from 'expo-font' 
+
+export default function App(props): JSX.Element {
+
+	const [isLoadingComplete, setLoadingComplete] = React.useState(false)
+
+	if (!isLoadingComplete && !props.skipLoadingScreen) {
+		return (
+			<AppLoading
+				startAsync={loadResourcesAsync}
+				onError={handleLoadingError}
+				onFinish={() => setLoadingComplete(true)}
+			/>
+		)
+	}
 	return (
 		<ApolloProvider client={client}>
 			<NavigationContainer>
@@ -18,4 +34,16 @@ export default function App() {
 			</NavigationContainer>
 		</ApolloProvider>
 	)
+}
+
+async function loadResourcesAsync() {
+	await Promise.all([
+		Font.loadAsync({
+			'circular-std': require('./assets/fonts/CircularStd-Medium.ttf')
+		})
+	])
+}
+
+function handleLoadingError(error) {
+	console.warn('\n', error)
 }
