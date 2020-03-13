8bc6f56caddc80cc6d52776364053015699c6d93
itzezc <chirub@foretag.co>
2020-03-13 21:59:46 +0000

    [app] Screen (sub) components export chaining

--- app/core/components/index.ts
+++ app/core/components/index.ts
@@ -1,11 +1,15 @@
 export { Container } from './Container'
 export { Screen } from './Screen'
 export { Calendar } from './Calendar'
-export { MiniCards } from './Home'
 export { Switch } from './Switch'
 export {
 	Card as ProfileCard,
 	List as ProfileList,
 	Back as ProfileBack
 } from './Profile'
+export {
+	MiniCards,
+	Action as HomeAction
+} from './Home'
 export { Icon } from './Icon'
+export { Button } from './Login'
