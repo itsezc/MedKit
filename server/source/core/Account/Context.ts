9e54bbf8799286f03d66364aae539c38e005770e
itzezc <chirub@foretag.co>
2020-03-20 10:39:01 +0000

    [server] New Account dir structure

---
+++ server/source/core/Account/Context.ts
@@ -0,0 +1,48 @@
+import { AUTH_TOKEN } from '../../authToken'
+import fetchAccount from './library/fetchAccount'
+import JWT from 'jsonwebtoken'
+
+export async function authUser(authorization: string | void) {
+	
+	const bearerLength: number = 'Bearer '.length
+
+	if (authorization
+		&& authorization !== 'undefined'
+		&& authorization.length > bearerLength) {
+
+		const token: string = authorization.slice(bearerLength)
+
+		const { ok, result }: { ok: boolean, result: any } = await new Promise(async (resolve) => {
+
+			JWT.verify(token, AUTH_TOKEN, (error, result) => {
+
+				if (error) {
+					resolve({
+						ok: false, 
+						result: error
+					})
+				} else {
+					resolve({
+						ok: true,
+						result
+					})
+				}
+
+			})
+		})
+
+		if (ok) {
+
+			const { id }: { id: string } = result
+			return await fetchAccount(id)
+
+		} else {
+
+			console.error(await result)
+
+		}
+	}
+
+	return null
+}
+
