126c5c126eaac98c2da3e7a42aa72721c8ca51a9
itzezc <chirub@foretag.co>
2020-03-20 10:39:28 +0000

    [server] fetchActivities date range #R1.6

--- server/source/core/Activities/fetchActivities.ts
+++ server/source/core/Activities/fetchActivities.ts
@@ -14,15 +14,18 @@ type Activity = {
 
 export async function fetchActivities({ id }: { id: string }): Promise<Activities[]> {
 
-	// @ts-ignore
-	const maxDays = Moment(Moment().add(7, 'days')).utc().format()
+	const today = Moment().set({ 'hour': 0, 'minute': 0, 'second': 0 }).utc().format()
+	const maxDays = Moment().add(7, 'days').utc().format()
 
 	const GET_ACTIVITIES: string = `
-		query getActivities($id: ID!, $maxDays: DateTime!) {
+		query getActivities($id: ID!, $maxDays: DateTime!, $today: DateTime!) {
 			Account(id: $id) {
 				activities(
 					filter: {
-						time_lt: $maxDays
+						time_lt: $maxDays,
+						AND: {
+							time_gte: $today
+						}
 					},
 					orderBy: time_ASC
 				) {
@@ -33,7 +36,7 @@ export async function fetchActivities({ id }: { id: string }): Promise<Activitie
 			}
 		}
 	`
-	const { Account: { activities } } = await query(GET_ACTIVITIES, { id, maxDays })
+	const { Account: { activities } } = await query(GET_ACTIVITIES, { id, maxDays, today })
 
 	const grouped: Activities[] = []
 
