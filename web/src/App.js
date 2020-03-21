cb7c0ecea26c89cacaef1bfd744deb2e33710bbe
Vrishtrix <vrishin_patel@outlook.com>
2020-03-21 23:27:58 +0530

    [web] Updated dependencies, added routing

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+//Components
+import Login from './pages/Login';
 
 export default () => {
       return(
             <>
-                  Hello React!
+                  <Switch>
+                        <Route exact path='/' component={ Login } />
+                  </Switch>
             </>
       );
 }
--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
-ReactDOM.render( <App />, document.getElementById('container'));
+ReactDOM.render( 
+      <Router>
+            <App />
+      </Router>, document.getElementById('container'));
