183dacc814bdfb2f94fd298292f815c64aa2132c
itzezc <chirub@foretag.co>
2020-03-22 11:55:53 +0000

    [voice] Bug fixes, identation and onStart()

--- voice/index.py
+++ voice/index.py
@@ -1,9 +1,10 @@
 import pyttsx3
 def onStart(name):
-    engine.say('Launching:',name)
-engine=pyttsx3.init()
-engine.setProperty('voice',voice[1].id)
-engine.setProperty('rate',125)
-engine.connect('started-utterance',onStart)
-engine.say('Under progress')
-engine.runAndwait()
+	engine = pyttsx3.init()
+	# engine.setProperty('voice',voice[1].id)
+	engine.setProperty('rate', 180)
+	# engine.connect('started-utterance',onStart)
+	engine.say('It works! Indentation and bugs fixed')
+	engine.runAndWait()
+
+onStart('Chiru')
