19fb2f0bbf9ae82608ad15f0c59a703455014fd6
itzezc <chirub@foretag.co>
2020-03-22 12:00:03 +0000

    [voice] #DEMO playing around with voices

--- voice/index.py
+++ voice/index.py
@@ -1,9 +1,9 @@
 import pyttsx3
 def onStart(name):
 	engine = pyttsx3.init()
-	# engine.setProperty('voice',voice[1].id)
+	voices = engine.getProperty('voices')
+	engine.setProperty('voice',voices[7].id)
 	engine.setProperty('rate', 180)
-	# engine.connect('started-utterance',onStart)
 	engine.say('It works! Indentation and bugs fixed')
 	engine.runAndWait()
 
