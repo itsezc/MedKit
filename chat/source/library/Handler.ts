b78aa2478c1d9505090502fa6d70c98aed3feb8c
itzezc <chirub@foretag.co>
2020-03-24 14:00:19 +0000

    [chat] Hander.ts #Notes w/ Emit examples
    Co-authored-by: Ankit Raj <ryder.raj.ankit@gmail.com>

--- chat/source/library/Handler.ts
+++ chat/source/library/Handler.ts
@@ -5,7 +5,7 @@ import { Event } from '../../../common/constants'
 import { Identify } from './identify'
 import { getDiseases } from './getDiseases'
 import { askQuestions } from './questions'
-import { createMessage } from './Message'
+import { Message } from './message'
 
 export async function Handler(Socket: SocketIO.Socket, Server: SocketIO.Server) {
 	
@@ -13,14 +13,33 @@ export async function Handler(Socket: SocketIO.Socket, Server: SocketIO.Server) 
 
 	console.log('Client connected with ID', id)
 
-	Server.emit(Event.MESSAGE, await createMessage('Hello what is wrong with you'))
-	Server.emit(Event.MESSAGE, await createMessage('Here is another message from the chatHandler', 1))
+	// .. create a screen on the mobile app that lets the user select their symptoms
+	// then the chat screen is loaded with a list of symptom ids
+	// then the symptom ids are passed on to the identify function
+	// then the diseases are organised into likely and unlikely and ordered on the mode
+	// then the questions are generated based on each disease of the previous step in order
 
-	Socket.on('requestDisconnect', () => Socket.disconnect())
+	Server.emit(Event.MESSAGE, new Message('Hello, welcome to MedKit, today we\'ll diagnose you for COVID-19'))
+	Server.emit(Event.MESSAGE, new Message('To begin, do you have a dry cough?', Event.MESSAGE, 1, {
+		type: 'checkbox',
+		values: [
+			{
+				title: 'Yes',
+				value: 'yes'
+			},
+			{
+				title: 'No',
+				value: 'no'
+			}
+		]
+	}))
 
+	Socket.on(Event.REQUEST_DISCONNECT, () => Socket.disconnect())
+	Socket.on(Event.MESSAGE, message => console.log(message))
+
+	// Stage 1-3
 	Socket.on(Event.IDENTIFY, Identify)
 	Socket.on(Event.GET_DISEASES, getDiseases)
-
 	Socket.on(Event.ASK_QUESTIONS, (diseases: string[]) => askQuestions(diseases, Server))
 
 	Socket.on(Event.DISCONNECT, () => {
