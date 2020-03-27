0782d1bcbc13aa3b86b3cacd1223711b6a2adf6d
itzezc <chirub@foretag.co>
2020-03-27 20:03:23 +0000

    [chat] Indentify / Stage 1 : #R1.5 - WORKING

--- chat/source/library/identify/index.ts
+++ chat/source/library/identify/index.ts
@@ -1,14 +1,23 @@
 import { query } from '../GClient'
 import { updateList } from '../../core/redis'
 
-export async function Identify({ symptoms }: { symptoms: string[] }, { id }: { id: string }): Promise<typeof symptoms> {
+export async function Identify(
+	{ symptoms }: { symptoms: string[] },
+	{ id }: { id: string },
+	Server: SocketIO.Server
+): Promise<void> {
 
-	const results: typeof symptoms = []
-	
-	symptoms.forEach(async(symptom) => {
+	let results: typeof symptoms = []
+
+	const response = async (results: typeof symptoms) => {
+		await updateList(`user_${id}`, results)
+		Server.emit('message', results)
+	} 
+
+	symptoms.forEach(async(symptom, index, array) => {
 
 		const FIND_DISEASES: string = `
-			query findDiseases($symptom: String!) {
+			query findDiseases($symptom: ID!) {
 				allDiseases(
 					filter: {
 						symptoms_some: {
@@ -20,13 +29,12 @@ export async function Identify({ symptoms }: { symptoms: string[] }, { id }: { i
 				}
 			}
 		`
-		const diseases: [{ id: string }] = await query(FIND_DISEASES, { symptom })
-	
-		diseases.forEach(({ id }) => results.push(id))
+		const { allDiseases }: { allDiseases: [{ id: string }] } = await query(FIND_DISEASES, { symptom })
 		
+		allDiseases.forEach(async ({ id }) => results.push(id))
+
+		if (index === array.length - 1) {
+			await response(results)
+		}
 	})
-
-	await updateList(`user_${id}`, results)
-
-	return results
 }
