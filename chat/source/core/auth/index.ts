f6b86fa9cbc01c3db71631c0f25006823f5028bf
itzezc <chirub@foretag.co>
2020-03-28 19:07:19 +0000

    [chat] Auth throughout session via Redis #R1.1
    With regards to
    https://github.com/itsezc/MedKit/issues/2

--- chat/source/core/auth/index.ts
+++ chat/source/core/auth/index.ts
@@ -1,14 +1,22 @@
 import { AUTH_TOKEN } from '../../../../server/source/authToken'
-import fetchAccount from '../../../../server/source/lib/Account/library/fetchAccount'
 import JWT from 'jsonwebtoken'
 
-export async function Auth({ token }: { token: string }, User: object): Promise<object> {
+import { redis, cache } from '../redis'
+
+export async function Auth({ token }: { token: string }, Socket: SocketIO.Socket, Server: SocketIO.Server): Promise<void> {
+
+	/**
+	 *  ![SECURITY THREAT]
+	 *  Man in the middle attack where token is stolen
+	 *  Likely to happen over HTTP / unlikely over HTTPS but possible
+	*/
+
 	JWT.verify(token, AUTH_TOKEN, async (error, result: any) => {
-		if (!error) {
+		if (result) {
 			const { id }: { id: string } = result
-			User = await fetchAccount(id)
+			cache.set(Socket.id, id, redis.print)
+			cache.get(Socket.id, (err, reply) => Server.emit('message', reply))
 		}
 	})
 
-	return User
 }
