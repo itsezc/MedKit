c23d29030293c8569f6f4223398bb960c9d164d1
itzezc <chirub@foretag.co>
2020-03-31 03:29:34 +0100

    [app] SocketIO requestAuth / Auth event
    Completing ( Issue https://github.com/itsezc/MedKit/issues/2)

--- app/core/chat/handler.ts
+++ app/core/chat/handler.ts
@@ -1,11 +1,16 @@
 import SocketIO from 'socket.io-client'
-	
+import { Auth } from '../auth/Auth'
+
 export class chatHandler {
 	
 	private readonly Socket = SocketIO('http://192.168.0.111:8087')
 	private setMessages: Function
 
 	constructor(setMessages) {
+		this.Socket.on('requestAuth', () => {
+			this.Socket.emit('auth', Auth.getToken())
+		})
+
 		this.Socket.on('message', message => setMessages(message))
 	}
 
