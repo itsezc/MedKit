d332f5dba9ae8d5a59e70fe88beaa2c4777f21af
itzezc <chirub@foretag.co>
2020-04-01 19:33:55 +0100

    [chat] Remove cache on disconnect

--- chat/source/events/disconnect/Disconnect.ts
+++ chat/source/events/disconnect/Disconnect.ts
@@ -3,6 +3,7 @@ import { IEvent } from '../../communication/socket'
 
 export default class Disconnection extends Event implements IEvent {
 	public async execute(): Promise<void> {
+		this.cache.DEL(this.socketID)
 		this.socket.disconnect(true)
 	}
 }
