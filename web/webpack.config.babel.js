9959a6faa9592d43ef9acbb26cbec2db2f5710c6
itzezc <chirub@foretag.co>
2020-04-01 20:01:09 +0100

    [web] REBASE w/ TypeScript

---
+++ web/webpack.config.babel.js
@@ -0,0 +1,38 @@
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+module.exports = (env, argv) => {
+	return {
+		plugins: [
+			new HtmlWebpackPlugin({
+				filename: 'index.html',
+				template: './public/index.html'
+			})
+		],
+
+		entry: {
+			App: './app/App.tsx'
+		},
+
+		resolve: {
+			extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+		},
+
+		devServer: {
+			compress: true,
+			historyApiFallback: true
+		},
+
+		module: {
+			rules: [
+				{
+					test: /\.(ts|tsx)$/,
+					exclude: /node_modules/,
+					use: [
+						'babel-loader'
+					]
+				},
+			]
+		}
+	}
+}
