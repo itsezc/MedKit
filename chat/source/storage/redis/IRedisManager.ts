f250306f000f7dd6f20a1b5e5d607a7d7448bdb2
itzezc <chirub@foretag.co>
2020-04-05 15:45:21 +0530

    [chat] RedisManager | Redis => HandyRedis

--- chat/source/storage/redis/IRedisManager.ts
+++ chat/source/storage/redis/IRedisManager.ts
@@ -1,8 +1,8 @@
-import { RedisClient } from 'redis'
+import { IHandyRedis } from 'handy-redis'
 
 export default interface IRedisManager {
 	init(): void
 	updateList(key: string, data: string[]): void
 	getList(key: string): Promise<string[]>
-	getClient(): RedisClient
+	getClient(): IHandyRedis
 }
--- chat/source/storage/redis/RedisManager.ts
+++ chat/source/storage/redis/RedisManager.ts
@@ -1,32 +1,31 @@
 import { injectable } from 'inversify'
 
-import Redis from 'redis'
+
+import { createHandyClient, IHandyRedis } from 'handy-redis'
 import IRedisManager from './IRedisManager'
 
 @injectable()
 export default class RedisManager implements IRedisManager {
 	
-	public client: Redis.RedisClient
+	public client: IHandyRedis
 
 	public init() {
-		this.client = Redis.createClient(6379, 'http://62.171.183.5')
-		this.client.on('error', (error) => console.error(error))
+		// this.client = Redis.createClient(6379, '62.171.183.5', { password: '1234567890' })
+		this.client = createHandyClient({ port: 6379, host: '62.171.183.5', password: '1234567890' })
+		// this.client.on('error', (error) => console.error(error))
 	}
 
 	public async updateList(key: string, data: string[]) {
-		this.client.DEL(key)
-		this.client.rpush(key, ...data)
+		await this.client.del(key)
+		await this.client.rpush(key, ...data)
 	}
 
 	public async getList(key: string): Promise<typeof results> {
-		const results: string[] = []
-		this.client.lrange(key, 0, -1, async (err, reply) => {
-			if (!err) reply.forEach(instance => results.push(instance))
-		})
+		let results = await this.client.lrange(key, 0, -1)
 		return results
 	}
 
-	public getClient(): Redis.RedisClient {
+	public getClient(): IHandyRedis {
 		return this.client
 	}
 }
