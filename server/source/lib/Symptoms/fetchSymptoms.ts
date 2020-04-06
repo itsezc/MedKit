0edd8a00c9493cd760bc16fe4eea2e2161d79b21
itzezc <chirub@foretag.co>
2020-04-06 20:18:47 +0530

    [server] fetchSymptoms with Context #R1.2

--- server/source/lib/Symptoms/fetchSymptoms.ts
+++ server/source/lib/Symptoms/fetchSymptoms.ts
@@ -1,6 +1,37 @@
 import { query } from '../../util/GClient'
 
-export async function fetchSymptoms() {
+async function filterSymptoms(current: string[]): Promise<typeof results> {
+ 	let results: Array<{ id: string, name: string }>
+	const GET_DISEASES: string = `
+		query getDiseasesSymptoms($current: [ID!]!) {
+			allDiseases (
+				filter: {
+					symptoms_every: {
+						id_in: $current
+					}
+				}
+			) {
+				symptoms (
+					filter: {
+						id_not_in: $current
+					}
+				) {
+					id
+					name
+				}
+			}
+		}
+	`
+	const { allDiseases }: { allDiseases: Array<{ symptoms: { id: string, name: string } }> }
+		= await query(GET_DISEASES, { current })
+	
+	allDiseases.forEach(async ({ symptoms }) => results.push(symptoms))
+	return results
+}
+
+
+export async function fetchSymptoms(current: string[]): Promise<typeof allSymptoms> {
+	if (current.length > 0) return await filterSymptoms(current)
 
 	const FETCH_SYMPTOMS = `
 		query {
@@ -12,12 +43,14 @@ export async function fetchSymptoms() {
 			}
 		}
 	`
-
-	const { allSymptoms } = await query(FETCH_SYMPTOMS)
-
+	const { allSymptoms }: { allSymptoms: Array<{ id: string, name: string }> } = await query(FETCH_SYMPTOMS)
 	return allSymptoms
 }
 
+
+/**
+ * @CHECK
+ */
 export async function fetchSymptom(name: string) {
 
 	const FETCH_SYMPTOM = `
