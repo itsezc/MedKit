bb27d0e2d58afec849da1f5630ab3c4217c49a1e
Vrishtrix <vrishin_patel@outlook.com>
2020-04-08 16:26:39 +0530

    [WEB] Reconfigured it to work

--- web/webpack.config.babel.js
+++ web/webpack.config.babel.js
@@ -45,6 +45,22 @@ module.exports = (env, argv) => {
 				{
 					test: /\.css$/,
 					use: ['style-loader', 'css-loader']
+				},
+				{
+					test: /\.less$/,
+					use: [{
+							loader: 'style-loader'
+						},
+						{
+							loader: 'css-loader'
+						},
+						{
+							loader: 'less-loader',
+							options: {
+								javascriptEnabled: true
+							}
+						}
+					]
 				}
 			]
 		}
