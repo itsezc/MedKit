96397576e9c7ec75e779910ea6b022fbee6bdab2
itsezc <chirub@foretag.co>
2020-04-10 13:38:33 +0200

    [app] Handler for Auth onConnect

--- app/core/chat/handler.ts
+++ app/core/chat/handler.ts
@@ -2,19 +2,22 @@ import SocketIO from 'socket.io-client'
 import { Auth } from '../auth/Auth'
 
 export class chatHandler {
-	
-	private readonly Socket = SocketIO('http://192.168.0.111:8087')
-	private setMessages: Function
+		private readonly Socket = SocketIO.connect('http://62.171.183.5:8087')
 
-	constructor(setMessages) {
-		this.Socket.on('requestAuth', () => {
-			this.Socket.emit('auth', Auth.getToken())
+	constructor(
+		private setMessages: Function
+	) { this.handle() }
+
+	async handle() {
+		this.Socket.on('connect', async () => {
+			const token = await Auth.getToken() 
+			this.Socket.emit('auth', { token })
+			this.Socket.on('authSuccess', () => console.log(' - APP Auth Confirmed'))
+			this.Socket.on('message', message => this.setMessages(message))
 		})
-
-		this.Socket.on('message', message => setMessages(message))
 	}
 
 	public disconnect() {
-		this.Socket.emit('requestDisconnect')
+		this.Socket.disconnect()
 	}
 }
