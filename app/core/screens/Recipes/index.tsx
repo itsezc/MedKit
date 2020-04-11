0db1a628c9d6178abfde8684421310c547fb9440
itsezc <chirub@foretag.co>
2020-04-11 23:20:28 +0200

    [app] Reciepe screen sub navigation

--- app/core/screens/Recipes/index.tsx
+++ app/core/screens/Recipes/index.tsx
@@ -4,16 +4,17 @@ import { View, Text } from 'react-native'
 import { Screen, Icon, RecipesCard as Card } from '../../components'
 import { ScrollView } from 'react-native-gesture-handler'
 
-export default function ({ navigation }): JSX.Element {
+export default function({ navigation }): JSX.Element {
 	return (
 		<Screen
-			background='#FFFFFF'
-			statusBar='#FFFFFF'
+			background='#F7F7F7'
+			statusBar='#F7F7F7'
 			statusBarColor='dark-content'
 		>
+			{/* Header */}
 			<View
 				style={{
-					marginTop: 30,
+					marginTop: 40,
 					marginHorizontal: 25,
 					flexDirection: 'row',
 					alignItems: 'center'
@@ -43,7 +44,7 @@ export default function ({ navigation }): JSX.Element {
 						<Icon
 							name='Search'
 							size='24'
-							color='#363946'
+							color='#AFB4C0'
 						/>
 					</View>
 
@@ -55,7 +56,7 @@ export default function ({ navigation }): JSX.Element {
 						<Icon
 							name='Filter3'
 							size='24'
-							color='#363946'
+							color='#AFB4C0'
 						/>
 					</View>
 					
@@ -65,13 +66,34 @@ export default function ({ navigation }): JSX.Element {
 						}}
 					>
 						<Icon
-							name='Heart2Line'
+							name='Heart2Fill'
 							size='24'
-							color='#363946'
+							color='#AFB4C0'
 						/>
 					</View>
 					
 				</View>			
+			</View>
+			{/* End Header */}
+
+			{/* Planner */}
+			<View
+				style={{
+					marginHorizontal: 25,
+					flexDirection: 'row',
+					alignItems: 'center',
+					marginTop: 20
+				}}
+			>
+				<Text
+					style={{
+						color: '#BCBDBE',
+						fontFamily: 'circular-std',
+						fontSize: 16,
+					}}
+				>
+					Daily Planner
+				</Text>
 			</View>
 
 			<ScrollView
@@ -79,13 +101,127 @@ export default function ({ navigation }): JSX.Element {
 				showsHorizontalScrollIndicator={false}
 				style={{
 					maxHeight: 250,
-					marginTop: 30,
+					marginTop: 10,
 					marginHorizontal: 25,
 				}}
 			>
-				<Card />
-				<Card />
+				<Card 
+					tag='Breakfast'
+				/>
+				<Card 
+					tag='Lunch'
+				/>
+				<Card 
+					tag='Dinner'
+				/>
 			</ScrollView>
+			{/* End Planner */}
+
+			{/* Featured */}
+			<View
+				style={{
+					marginHorizontal: 25,
+					flexDirection: 'row',
+					alignItems: 'center',
+					marginTop: 40
+				}}
+			>
+				<Text
+					style={{
+						color: '#BCBDBE',
+						fontFamily: 'circular-std',
+						fontSize: 16
+					}}
+				>
+					Featured
+				</Text>
+
+				<Text
+					style={{
+						marginLeft: 'auto',
+						marginRight: 5,
+						fontSize: 18,
+						fontWeight: 'bold',
+						color: '#E69C59'
+					}}
+				>All</Text>
+
+				<Icon 
+					name='ArrowRightLine'
+					size='18'
+					color='#E69C59'
+				/>
+			</View>
+
+			<ScrollView
+				horizontal
+				showsHorizontalScrollIndicator={false}
+				style={{
+					maxHeight: 250,
+					marginTop: 10,
+					marginHorizontal: 25,
+				}}
+			>
+				<Card
+					favorite
+					featured
+				/>
+				<Card 
+					featured
+				/>
+				<Card 
+					featured
+				/>
+			</ScrollView>
+			{/* End Featured */}
+
+			<View
+				style={{
+					marginTop: 'auto',
+					backgroundColor: '#FFFFFF',
+					borderTopColor: '#F3F3F3',
+					borderTopWidth: 2,
+					paddingVertical: 15,
+					paddingHorizontal: 25,
+					flexDirection: 'row',
+					justifyContent: 'space-evenly'
+				}}
+			>
+				<View
+					style={{
+						justifyContent: 'center',
+						alignItems: 'center'
+					}}
+				>
+					<Icon
+						name='Home8Line'
+						size='24'
+						color='#AFB4C0'
+					/>
+					<Text
+						style={{
+							fontSize: 8
+						}}
+					>Home</Text>
+				</View>
+				<View
+					style={{
+						justifyContent: 'center',
+						alignItems: 'center'
+					}}
+				>
+					<Icon
+						name='Home8Line'
+						size='24'
+						color='#AFB4C0'
+					/>
+					<Text
+						style={{
+							fontSize: 8
+						}}
+					>Home</Text>
+				</View>
+			</View>
 		</Screen>
 	)
 }
