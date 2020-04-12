8429eaccedd9ca51b7cce5fd99aa3beee613615f
itzezc <chirub@foretag.co>
2020-04-12 19:00:09 +0530

    [server] Typings for Resolvers and ResolverMap

--- packages/server/typings/graphql.d.ts
+++ packages/server/typings/graphql.d.ts
@@ -4,3 +4,25 @@ declare module '*.graphql' {
     
 	export = Schema
 }
+
+export interface Context {
+	// url: string;
+	// session: Session;
+	// req: Express.Request;
+	// res: express.Response;
+	// userLoader: ReturnType<typeof userLoader>;
+	// pubsub: PubSub;
+}
+
+export type Resolver = (
+	parent: any,
+	args: any,
+	// context: Context,
+	info: any
+) => any;
+
+export interface ResolverMap {
+	[key: string]: {
+	  [key: string]: Resolver | { [key: string]: Resolver };
+	};
+}
