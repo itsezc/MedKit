4e2987a905dc63762aaf81e297208760d3fc0567
itzezc <chirub@foretag.co>
2020-04-12 17:57:16 +0530

    [server] #RF2 server container

--- packages/server/source/server.ts
+++ packages/server/source/server.ts
@@ -1,11 +1,26 @@
 import 'reflect-metadata'
 import { inject, injectable } from 'inversify'
 
+import { SERVICE_IDENTIFIER } from './includes/identifiers'
+import { IFederationManager } from './communication/graphql/federation/IFederationManager'
+import { IDatabaseManager } from './communication/database/IDatabaseManager'
+
 @injectable()
 export default class Server {
 	public static readonly DEBUG: boolean = true
 
+	public ORMServer: IDatabaseManager
+	public authServer: IFederationManager
+
 	public constructor(
-		@inject()
-	) {}
+		@inject(SERVICE_IDENTIFIER.IDatabaseManager) ORMServer: IDatabaseManager,
+		@inject(SERVICE_IDENTIFIER.IFederationManager) authServer: IFederationManager
+	) {
+		this.authServer = authServer
+		this.ORMServer = ORMServer
+	}
+
+	init() {
+		this.ORMServer.start()
+	}
 }
