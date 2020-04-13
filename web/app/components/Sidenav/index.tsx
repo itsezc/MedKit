2be06b49689f7cbfa379525a8851836b16d9fbed
Vrishtrix <vrishin_patel@outlook.com>
2020-04-13 18:39:04 +0530

    [WEB] Removed unwanted <Layout> and <Section>

--- web/app/components/Sidenav/index.tsx
+++ web/app/components/Sidenav/index.tsx
@@ -11,36 +11,32 @@ export default (props:any) => {
 
       return (
             <>
-                  <Layout>
-                        <Sider collapsed={true} style={{ height: '100vh' }}>
-                              <div className='logo' />
+                  <div className='logo' />
 
-                              <Menu
-                                    theme='dark'
-                                    defaultSelectedKeys={props.active}
-                                    mode='inline'
-                              >
-                                    <Menu.Item key='1'>
-                                          <HomeOutlined />
-                                          <span> Dashboard </span>
-                                    </Menu.Item>
+                  <Menu
+                        theme='dark'
+                        defaultSelectedKeys={props.active}
+                        mode='inline'
+                  >
+                        <Menu.Item key='1'>
+                              <HomeOutlined />
+                              <span> Dashboard </span>
+                        </Menu.Item>
 
-                                    <Menu.Item key='2'>
-                                          <FormOutlined />
-                                          <span> Reports </span>
-                                    </Menu.Item>
+                        <Menu.Item key='2'>
+                              <FormOutlined />
+                              <span> Reports </span>
+                        </Menu.Item>
 
-                                    <div className='side-bottom'>
-                                          <Avatar 
-                                                className='logout-btn'
-                                                shape='square' 
-                                                size='large'
-                                                icon={<LogoutOutlined />} 
-                                          />
-                                    </div>
-                              </Menu>
-                        </Sider>
-                  </Layout>
+                        <div className='side-bottom'>
+                              <Avatar 
+                                    className='logout-btn'
+                                    shape='square' 
+                                    size='large'
+                                    icon={<LogoutOutlined />} 
+                              />
+                        </div>
+                  </Menu>
             </>
       );
 }
