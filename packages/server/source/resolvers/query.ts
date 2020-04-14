dfa2bd1b23fbeec182c66a2b7b9f4b04d6ec2243
itzezc <chirub@foretag.co>
2020-04-14 15:09:20 +0530

    [server] Schema and resolver for favoriteRecipes

--- packages/server/source/resolvers/query.ts
+++ packages/server/source/resolvers/query.ts
@@ -2,6 +2,8 @@ import { fetchActivities } from '../lib/Activities'
 import { fetchSymptoms, fetchSymptom } from '../lib/Symptoms'
 import { searchMedicine, topMedicines } from '../lib/Medicines'
 
+import { fetchFavorites } from '../lib/Recipes'
+
 export const queryResolver = {
 
 	getUser: async (parent, args, { user }, info) => user,
@@ -10,6 +12,8 @@ export const queryResolver = {
 	getSymptom: async (parent, { current, name }: { current: string [], name: string }, context, info) => fetchSymptom(current, name),
 	
 	searchMedicine: async (parent, args, context, info) => searchMedicine(args),
-	topMedicines: async (parent, args, context, info) => topMedicines({ limit: 10 })
+	topMedicines: async (parent, args, context, info) => topMedicines({ limit: 10 }),
+
+	getFavoriteRecipes: async (parent, args, { user }, info) => fetchFavorites(user),
 	
 }
