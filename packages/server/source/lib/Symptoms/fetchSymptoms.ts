32351f1a5b0989429a41a75345b428344993e35f
itzezc <chirub@foretag.co>
2020-04-15 20:32:05 +0530

    [server] fetchSymptoms Cleanup #3

--- packages/server/source/lib/Symptoms/fetchSymptoms.ts
+++ packages/server/source/lib/Symptoms/fetchSymptoms.ts
@@ -1,38 +1,6 @@
 import { query } from '../../util/GClient'
 import { filterSymptoms } from '../../flux/models/Symptoms/filterSymptoms'
 
-// export async function filterSymptoms(current: string[], name?: string): Promise<typeof results> {
-// 	let results: Array<{ id: string, name: string }> = []
-// 	const GET_DISEASES: string = `
-// 		query getDiseasesSymptoms($current: [ID!], $name: String) {
-// 			allDiseases (
-// 				filter: {
-// 					symptoms_some: {
-// 						id_in: $current
-// 					}
-// 				}
-// 			) {
-// 				symptoms (
-// 					filter: {
-// 						id_not_in: $current,
-// 						AND: {
-// 							name_contains: $name
-// 						}
-// 					}
-// 				) {
-// 					id
-// 					name
-// 				}
-// 			}
-// 		}
-// 	`
-// 	const { allDiseases }: { allDiseases: /* Array<{ symptoms: { id: string, name: string } }> */ any }
-// 		= await query(GET_DISEASES, { current, name })
-	
-// 	allDiseases.forEach(({ symptoms }: { symptoms: any }) => symptoms.forEach((symptom: any) => results.push(symptom)))
-// 	return results
-// }
-
 export async function fetchSymptoms(current: string[]): Promise<typeof allSymptoms> {
 	if (current !== undefined
 		&& current.length > 0) return await filterSymptoms(current)
