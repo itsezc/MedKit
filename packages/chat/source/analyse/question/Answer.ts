42772fa8ff11925f119cecf80d32222e99640612
itzezc <chirub@foretag.co>
2020-04-17 01:54:05 +0530

    [chat] Answer questionAsk

--- packages/chat/source/analyse/question/Answer.ts
+++ packages/chat/source/analyse/question/Answer.ts
@@ -1,11 +1,11 @@
 import { Event } from '../../events/Abstract'
 import { IEvent } from '../../communication/socket'
-import { query } from '../../util/GClient'
+import { getDiseases } from '../../../../server/source/flux/models/Symptoms/getDiseases'
 
 import { compileMessage } from '../../messaging/Message'
 
 export default class Answer extends Event implements IEvent {
-	public async execute({ to, answer }: { to: string, answer: string}) {
+	public async execute({ to, answer }: { to: string, answer: string }) {
 		super.auth()
 		const symptoms = this.socketID.concat('_symptoms')
 		const askSymptoms = this.socketID.concat('_ask_symptoms')
@@ -18,55 +18,36 @@ export default class Answer extends Event implements IEvent {
 		await this.cache.lrem(askSymptoms, 0, to)
 		const next = await this.cache.lrange(askSymptoms, 0, 0)
 
-		// if (next.length < 1) /** Evaluate final */
-		// else /** Ask next question */
+		if (next.length < 1) return
+		else { this.ask(next[0]) }
 	}
 
-	private async getTree(current: string[]) {
-		const tree = {}
-		const setDeep = async (obj: object, path: string[], value: any, setrecursively = false) => {
-			path.reduce((a, b, level) => {
-				if (setrecursively && typeof a[b] === 'undefined' && level !== path.length) {
-					a[b] = {}
-					return a[b]
-				}
-				if (level === path.length){
-					a[b] = value
-					return value
-				} 
-				return a[b]
-			}, obj)
-		}
-
-		current.forEach((symptom, index, array) => {
-			const depth = []
-			for (var i = 0; i < index; i++) { depth.push('AND') }
-			await setDeep(tree, depth, { 
-				symptoms_some: {
-					name: symptom
-				}
-			}, true)
+	private async ask(next: string[]) {
+		const messages = await this.cache.hget(this.socketID, 'messages')
+		const index = messages ? parseInt(messages) : 0
+		compileMessage({
+			text: 'Do you suffer from' + next[0],
+			index,
+			quickReplies: {
+				type: 'radio',
+				values: [
+					{
+						title: 'Yes',
+						value: 'Yes'
+					},
+					{
+						title: 'No',
+						value: 'No'
+					}
+				]
+			}
 		})
 	}
 
 	private async check(current: string[]) {
-		const tree = await this.getTree(current)
-		console.log('- DEBUG (tree):', tree)
-		const GET_DISEASES: string = `
-			query getDiseases($current: [ID]!) {
-				allDiseases(
-					filter: {
-						${tree}
-					}
-				) {
-					id
-					name
-				}
-			}
-		`
-		const { allDiseases } = await query(GET_DISEASES, { current })
+		const diseases = await getDiseases(current)
 
-		// if (allDiseases.length === 1)
+		if (diseases.length === 1)
 			/** Found one likely disease */
 		// else if (allDiseases.length < 1)
 			/** No diseases */
