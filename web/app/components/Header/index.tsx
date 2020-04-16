3ed6f528b5834cc8c0b117aaef4d9fe5b380aba3
Vrishtrix <vrishin_patel@outlook.com>
2020-04-16 09:17:02 +0530

    [WEB] Removed unwanted code, re-referenced styles

--- web/app/components/Header/index.tsx
+++ web/app/components/Header/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 
-import { Layout } from 'antd';
-
 //icons
 import { UserOutlined } from '@ant-design/icons';
 
 export default (props:any) => {
-
-      const { Header } = Layout;
 
       return(
             <>
