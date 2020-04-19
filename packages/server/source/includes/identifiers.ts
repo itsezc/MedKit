13e0eed57e26ca128fe6a5d5c20fd38c85d4eef7
itzezc <chirub@foretag.co>
2020-04-19 18:51:28 +0530

    [server] Redis and EventManager Identifiers

--- packages/server/source/includes/identifiers.ts
+++ packages/server/source/includes/identifiers.ts
@@ -4,6 +4,8 @@ const SERVICE_IDENTIFIER= {
 	IFederationManager: Symbol.for('FederationManager'),
 	IHTTPManager: Symbol.for('HTTPManager'),
 	ISocketManager: Symbol.for('SocketManager'),
+	IRedisManager: Symbol.for('RedisManager'),
+	IEventManager: Symbol.for('EventManager'),
 }
 
 export { SERVICE_IDENTIFIER }
