32e5324ae6bec55fbb0d0b90188e5fd9ca5bbf71
itzezc <chirub@foretag.co>
2020-04-19 19:01:38 +0530

    [server] injector for Socket, Apollo + Events

--- packages/server/source/includes/injectors/ServerContainer.ts
+++ packages/server/source/includes/injectors/ServerContainer.ts
@@ -3,8 +3,20 @@ import { Container } from 'inversify'
 import { SERVICE_IDENTIFIER } from '../identifiers'
 
 import Server from '../../server'
+import { SocketManager, ISocketManager, IEventManager, EventManager } from '../../communication/socket'
+import { IHTTPManager, HTTPManager } from '../../communication/http'
+import { IRedisManager, RedisManager } from '../../storage/redis'
+import { QueryManager, IQueryManager } from '../../communication/graphql/query'
+import { IFederationManager, FederationManager } from '../../communication/graphql/federation'
+import { IGatewayManager, GatewayManager } from '../../communication/graphql/gateway'
 
 const ServerContainer = new Container()
 ServerContainer.bind<Server>(Server).toSelf().inRequestScope()
-
+ServerContainer.bind<IHTTPManager>(SERVICE_IDENTIFIER.IHTTPManager).to(HTTPManager).inSingletonScope()
+ServerContainer.bind<ISocketManager>(SERVICE_IDENTIFIER.ISocketManager).to(SocketManager).inSingletonScope()
+ServerContainer.bind<IFederationManager>(SERVICE_IDENTIFIER.IFederationManager).to(FederationManager).inSingletonScope()
+ServerContainer.bind<IGatewayManager>(SERVICE_IDENTIFIER.IGatewayManager).to(GatewayManager).inSingletonScope()
+ServerContainer.bind<IEventManager>(SERVICE_IDENTIFIER.IEventManager).to(EventManager).inSingletonScope()
+ServerContainer.bind<IRedisManager>(SERVICE_IDENTIFIER.IRedisManager).to(RedisManager).inSingletonScope()
+ServerContainer.bind<IQueryManager>(SERVICE_IDENTIFIER.IQueryManager).to(QueryManager).inSingletonScope()
 export { ServerContainer }
