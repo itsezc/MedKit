9aba716e2dc8c8b608ac997b4c0258b3106664ee
itzezc <chirub@foretag.co>
2020-04-19 17:31:36 +0530

    [server] SERVICE_IDENTIFIERS HTTP & Socket

--- packages/server/source/includes/identifiers.ts
+++ packages/server/source/includes/identifiers.ts
@@ -1,7 +1,9 @@
 const SERVICE_IDENTIFIER= {
 	IDatabaseManager: Symbol.for('DatabaseManager'),
 	IGatewayManager: Symbol.for('GatewayManager'),
-	IFederationManager: Symbol.for('FederationManager')
+	IFederationManager: Symbol.for('FederationManager'),
+	IHTTPManager: Symbol.for('HTTPManager'),
+	ISocketManager: Symbol.for('SocketManager'),
 }
 
 export { SERVICE_IDENTIFIER }
