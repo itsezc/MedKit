d0c2385c56bcce6437bbc24a3ebef6d8141d3c5e
itsec <chirub@foretag.co>
2020-04-20 16:19:15 +0200

    [app] FeaturedRecipes query load state

--- packages/app/core/components/Recipes/Featured.tsx
+++ packages/app/core/components/Recipes/Featured.tsx
@@ -17,60 +17,65 @@ const GET_FEATURED_RECIPES = gql`
 export function FeaturedRecipes() {
 	const { data, error, loading } = useQuery(GET_FEATURED_RECIPES)
 
-	return (
-		<>
-			<View
-				style={{
-					marginHorizontal: 25,
-					flexDirection: 'row',
-					alignItems: 'center',
-					marginTop: 40
-				}}
-			>
-				<Text
+	if (data) {
+		return (
+			<>
+				<View
 					style={{
-						color: '#BCBDBE',
-						fontFamily: 'circular-std',
-						fontSize: 16
+						marginHorizontal: 25,
+						flexDirection: 'row',
+						alignItems: 'center',
+						marginTop: 40
 					}}
 				>
-					Featured
-				</Text>
-
-				<Text
-					style={{
-						marginLeft: 'auto',
-						marginRight: 5,
-						fontSize: 18,
-						fontWeight: 'bold',
-						color: '#E69C59'
-					}}
-				>All</Text>
-
-				<Icon 
-					name='ArrowRightLine'
-					size='18'
-					color='#E69C59'
-				/>
-			</View>
-
-			<ScrollView
-				horizontal
-				showsHorizontalScrollIndicator={false}
-				style={{
-					maxHeight: 250,
-					marginTop: 10,
-					marginHorizontal: 25,
-				}}
-			>
-				{data.getRecipes.map(({ name, preview }) => (
-					<Card
-						name={name}
-						preview={preview}
-						featured
+					<Text
+						style={{
+							color: '#BCBDBE',
+							fontFamily: 'circular-std',
+							fontSize: 16
+						}}
+					>
+						Featured
+					</Text>
+	
+					<Text
+						style={{
+							marginLeft: 'auto',
+							marginRight: 5,
+							fontSize: 18,
+							fontWeight: 'bold',
+							color: '#E69C59'
+						}}
+					>All</Text>
+	
+					<Icon 
+						name='ArrowRightLine'
+						size='18'
+						color='#E69C59'
 					/>
-				))}
-			</ScrollView>
-		</>
-	)
+				</View>
+	
+				<ScrollView
+					horizontal
+					showsHorizontalScrollIndicator={false}
+					style={{
+						maxHeight: 250,
+						marginTop: 10,
+						marginHorizontal: 25,
+					}}
+				>
+					{data.getRecipes.map(({ id, name, preview }) => (
+						<Card
+							id={id}
+							name={name}
+							preview={preview}
+							featured
+						/>
+					))}
+				</ScrollView>
+			</>
+		)
+	} else {
+		return null
+	}
 }
