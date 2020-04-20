ca22eacf4385d83a40fc3e3a7349cc5944aeb04d
itsec <chirub@foretag.co>
2020-04-20 16:19:56 +0200

    [server] isFavoriteRecipe and getRecipes methods

--- packages/server/source/lib/Recipes/index.ts
+++ packages/server/source/lib/Recipes/index.ts
@@ -1,6 +1,34 @@
 import { params, types, query, mutation } from 'typed-graphqlify'
 import { request } from 'graphql-request'
 
+export async function isFavoriteRecipe({ account, recipe }: { account: string, recipe: string }) {
+	const favorites: string[] = []
+	const IS_FAVORITE =  query('isFavorite', {
+		Account: params({ id: account}, {
+			recipes: [
+				{
+					id: types.string
+				}
+			]
+		})
+	})
+	const { Account: { recipes }} = await request('http://localhost:8085', IS_FAVORITE)
+	await recipes.forEach(({ id }: { id: string }) => favorites.push(id))
+	return favorites.includes(recipe)
+}
+
+export async function getRecipes() {
+	const GET_RECIPES = query('getRecipes', {
+		allRecipes: {
+			id: types.string,
+			name: types.string,
+			preview: types.string
+		}
+	})
+	const { allRecipes } = await request('http://localhost:8085/graphql', GET_RECIPES)
+	return allRecipes
+}
+
 export async function fetchFavorites({ id }: { id: string }) {
 	const GET_FAVORITES = {
 		Account: params(
