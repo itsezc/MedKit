a0832074181dff0f2b28df0b14e69d35dc63509c
itzezc <chirub@foretag.co>
2020-04-20 17:27:18 +0530

    [server] DBManager Injector

--- packages/server/source/includes/injectors/ServerContainer.ts
+++ packages/server/source/includes/injectors/ServerContainer.ts
@@ -9,11 +9,13 @@ import { IRedisManager, RedisManager } from '../../storage/redis'
 import { QueryManager, IQueryManager } from '../../communication/graphql/query'
 import { IFederationManager, FederationManager } from '../../communication/graphql/federation'
 import { IGatewayManager, GatewayManager } from '../../communication/graphql/gateway'
+import { IDatabaseManager, DatabaseManager } from '../../communication/database'
 
 const ServerContainer = new Container()
 ServerContainer.bind<Server>(Server).toSelf().inRequestScope()
 ServerContainer.bind<IHTTPManager>(SERVICE_IDENTIFIER.IHTTPManager).to(HTTPManager).inSingletonScope()
 ServerContainer.bind<ISocketManager>(SERVICE_IDENTIFIER.ISocketManager).to(SocketManager).inSingletonScope()
+ServerContainer.bind<IDatabaseManager>(SERVICE_IDENTIFIER.IDatabaseManager).to(DatabaseManager).inSingletonScope()
 ServerContainer.bind<IFederationManager>(SERVICE_IDENTIFIER.IFederationManager).to(FederationManager).inSingletonScope()
 ServerContainer.bind<IGatewayManager>(SERVICE_IDENTIFIER.IGatewayManager).to(GatewayManager).inSingletonScope()
 ServerContainer.bind<IEventManager>(SERVICE_IDENTIFIER.IEventManager).to(EventManager).inSingletonScope()
