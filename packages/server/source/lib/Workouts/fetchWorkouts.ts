08216abd394989012429d68df0765874f4211dc2
itzezc <chirub@foretag.co>
2020-04-26 23:17:09 +0530

    [server] fetchWorkout(Plan) #R1.1
    With typed-graphlify

--- packages/server/source/lib/Workouts/fetchWorkouts.ts
+++ packages/server/source/lib/Workouts/fetchWorkouts.ts
@@ -1,13 +1,12 @@
-import gql from 'graphql-tag'
-import { query } from '../../util/GClient'
+import { params, types, query, mutation } from 'typed-graphqlify'
+import { request } from 'graphql-request'
 
 export async function fetchWorkout({ id }: { id: string }) {
-	const FETCH_WORKOUT: string = `
-		query fetchWorkout($id: ID!) {
-			Workout(id: $id) {
-				name
-			}
-		}
-	`
-	
+	const FETCH_WORKOUT = query('fetchWorkout', {
+		Workout: params({ id }, {
+			name: types.string
+		})
+	})
+	const { Workout } = await request('https://localhost:8085', FETCH_WORKOUT)
+	return Workout
 }
